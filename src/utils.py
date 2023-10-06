04cde34d877cea8a3ee2505e932c846c736e847c
ZachNagengast <znagengast@gmail.com>
2023-10-06 11:42:00 -0700

    Allow initial dataset to be empty

--- src/utils.py
+++ src/utils.py
@@ -15,7 +15,16 @@ def get_user_headers() -> Dict[str, str]:
 
 def load_config() -> Dict[str, Any]:
     with open('config.json', 'r') as f:
-        return json.load(f)
+        config = json.load(f)
+    
+    # Override 'hf_dataset_name' if it exists in environment variables
+    env_hf_dataset_name = os.getenv('HF_DATASET_NAME')
+    if env_hf_dataset_name is not None:
+        config['hf_dataset_name'] = env_hf_dataset_name
+
+    print(f"Loaded config: {config}")
+
+    return config
     
 def parse_message(message: Dict[str, Any]) -> List[Tuple[str, str]]:
     # Assumes that message has the following format:
@@ -32,14 +41,20 @@ def prepare_dataset(messages: List[Tuple[str, str]], config) -> Dataset:
         "link": [image_url for _, image_url in messages]
     })
 
-    current_dataset = load_dataset(config['hf_dataset_name'])
+    # load the current dataset from the hub if exists
+    try:
+        current_dataset = load_dataset(config['hf_dataset_name'])
+    except:
+        current_dataset = None
+
+
     merged_dataset = merge_datasets(current_dataset, dataset)
 
     return merged_dataset
 
 def merge_datasets(old_dataset: Dataset, new_dataset: Dataset) -> Dataset:
-    # Gather existing URLs from old_dataset using indices
-    existing_image_urls = old_dataset['train']['link']
+    # Gather existing URLs from old_dataset using indices if it exists
+    existing_image_urls = old_dataset['train']['link'] if old_dataset is not None else []
 
     print(f"Current rows: {len(existing_image_urls)}")
 
@@ -48,7 +63,10 @@ def merge_datasets(old_dataset: Dataset, new_dataset: Dataset) -> Dataset:
     print(f"Rows to add: {filtered_new_dataset.num_rows}")
     
     # Concatenate the old and filtered new datasets
-    merged_dataset = concatenate_datasets([old_dataset['train'], filtered_new_dataset])
+    if old_dataset is None:
+        merged_dataset = filtered_new_dataset
+    else:
+        merged_dataset = concatenate_datasets([old_dataset['train'], filtered_new_dataset])
 
     print(f"New rows: {merged_dataset.num_rows}")
     return merged_dataset
